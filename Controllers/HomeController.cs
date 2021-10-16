4786cea805f23032f2834265e3287b1653f30ddb
LiamBartolini <liambartolini@gmail.com>
2021-10-16 08:18:35 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using bartolini.liam._5h.PrimaWeb.Models;
+
+namespace bartolini.liam._5h.PrimaWeb.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Persone()
+    {
+      List<Persona> p = new();
+      p.Add(new() { ID = 1, Nome = "Liam", Cognome = "Bartolini" });
+      p.Add(new() { ID = 2, Nome = "William", Cognome = "Lu" });
+
+      return View(p);
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bartolini.liam._5h.PrimaWeb.Models
+{
+  public class Persona
+  {
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public string Cognome { get; set; }
+  }
+}
